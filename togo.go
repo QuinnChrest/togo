192df22af4369ba528b989f2d5b6f7ab40d19b8d
Quinn Chrest <quinnchr@gmail.com>
2024-06-28 23:43:01 -0500

    progress

--- togo.go
+++ togo.go
@@ -5,36 +5,83 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"golang.org/x/term"
 )
 
 var (
-	titleStyle = func() lipgloss.Style {
-		b := lipgloss.RoundedBorder()
-		b.Right = "├"
-		return lipgloss.NewStyle().BorderStyle(b).Padding(0, 1)
-	}()
-
-	infoStyle = func() lipgloss.Style {
-		b := lipgloss.RoundedBorder()
-		b.Left = "┤"
-		return titleStyle.BorderStyle(b)
-	}()
+    header = lipgloss.NewStyle().
+        Bold(true).
+        Foreground(lipgloss.Color("#FAFAFA")).
+        Background(lipgloss.Color("#7D56F4")).
+        Height(1)
 )
 
 type Model struct {
     Items []Task
-    Cursor int
-    ViewPort viewport.Model
+    cursor int
+    add bool
+    textInput textinput.Model
+    viewPort viewport.Model
 }
 
 type Task struct {
     Description string
     Checked bool
+}
+
+func initialModel() Model{
+    // initialize text input
+    ti := textinput.New()
+	ti.Placeholder = "Add Task"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 20
+
+    w, h, err := term.GetSize(int(os.Stdout.Fd()))
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    vp := viewport.New(w, h-1)
+    vp.YPosition = 2
+
+    return Model{
+        Items: getItemsFromFile(),
+        add: false,
+        textInput: ti,
+        viewPort: vp,
+    }
+}
+
+func getItemsFromFile() []Task{
+    var list []Task
+
+    // read in the contents of json file or create one if one doesn't exist
+    file, err := os.Open("data.json")
+    if err != nil {
+        err := os.WriteFile("data.json", []byte(""), 0666)
+        if err != nil {
+            log.Fatal(err)
+        } else {
+            list = []Task{}
+        }
+    } else {
+        json.NewDecoder(file).Decode(&list)
+    }
+    defer file.Close()
+
+    return list
+}
+
+func addItem(model *Model) {
+    model.add = false
+    model.Items = append(model.Items, Task{Description: model.textInput.Value(), Checked: false})
+    model.textInput.SetValue("")
 }
 
 func (model Model) Init() tea.Cmd {
@@ -43,80 +90,108 @@ func (model Model) Init() tea.Cmd {
 }
 
 func (model Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+    var cmd tea.Cmd
+
     switch msg := msg.(type) {
 
-    // Is it a key press?
-    case tea.KeyMsg:
+        // Is it a key press?
+        case tea.KeyMsg:
 
-        // Cool, what was the actual key pressed?
-        switch msg.String() {
+            // Cool, what was the actual key pressed?
+            switch msg.String() {
 
-        // These keys should exit the program.
-        case "ctrl+c", "q":
-            saveTasks(model)
-            return model, tea.Quit
-
-        // The "up" and "k" keys move the cursor up
-        case "up", "k":
-            if model.Cursor > 0 {
-                model.Cursor--
+                // These keys should exit the program.
+                case "ctrl+c", "esc":
+                    if !model.add {
+                        saveTasks(model)
+                        return model, tea.Quit
+                    } else {
+                        model.textInput.SetValue("")
+                        model.add = false 
+                    }
+    
+                // The "up" and "k" keys move the cursor up
+                case "up", "k":
+                    if model.cursor > 0 && !model.add {
+                        model.cursor--
+                    }
+    
+                case "enter":
+                    if !model.add {
+                        model.Items[model.cursor].Checked = !model.Items[model.cursor].Checked
+                    } else {
+                        addItem(&model)
+                    }
+    
+                case "a":
+                    if !model.add {
+                        model.add = true
+                        model.textInput, cmd = model.textInput.Update(tea.KeyMsg{})
+                        return model, cmd
+                    }
+    
+                // The "down" and "j" keys move the cursor down
+                case "down", "j":
+                    if model.cursor < len(model.Items)-1 && !model.add {
+                        model.cursor++
+                    }
             }
 
-        case "enter":
-            model.Items[model.Cursor].Checked = !model.Items[model.Cursor].Checked
+        case tea.WindowSizeMsg:
+            if model.viewPort.Height != msg.Height - 1 || model.viewPort.Width != msg.Width {
+                model.viewPort.Height = msg.Height - 1
+                model.viewPort.Width = msg.Width
 
-        // The "down" and "j" keys move the cursor down
-        case "down", "j":
-            if model.Cursor < len(model.Items)-1 {
-                model.Cursor++
+                model.textInput.Width = model.viewPort.Width
             }
-        }
     }
 
-    // Return the updated model to the Bubble Tea runtime for processing.
-    // Note that we're not returning a command.
-    return model, nil
+    if !model.add {
+        return model, nil
+    } else {
+        model.textInput, cmd = model.textInput.Update(msg)
+        return model, cmd
+    }
 }
 
 func (model Model) View() string {
-    // The header
-    s := "What should we buy at the market?\n\n"
+    output := header.Width(model.viewPort.Width).Render("ToGo")
 
-    // Iterate over our choices
-    for i, item := range model.Items {
+    if !model.add {
+        content := ""
 
-        // Is the cursor pointing at this choice?
-        cursor := " " // no cursor
-        if model.Cursor == i {
-            cursor = ">" // cursor!
+        // Iterate over our choices
+        for i, item := range model.Items {
+
+            // Is the cursor pointing at this choice?
+            cursor := " " // no cursor
+            if model.cursor == i {
+                cursor = ">" // cursor!
+            }
+    
+            checked := " "
+            if(item.Checked){
+                checked = "x"
+            }
+    
+            // Render the row
+            content += fmt.Sprintf("%s [%s] %s", cursor, checked, item.Description)
+            if i < len(model.Items) - 1 {
+                content += "\n"
+            }
         }
 
-        checked := " "
-        if(item.Checked){
-            checked = "x"
-        }
-
-        // Render the row
-        s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, item.Description)
+        model.viewPort.SetContent(content)
+        output += model.viewPort.View()
+    } else {
+        output += model.textInput.View()
     }
 
     // The footer
-    s += "\nPress q to quit.\n"
+    output += header.Width(model.viewPort.Width).Render("Press Esc to quit.")
 
     // Send the UI for rendering
-    return s
-}
-
-func (model Model) headerView() string {
-	title := titleStyle.Render("Mr. Pager")
-	line := strings.Repeat("─", max(0, model.ViewPort.Width-lipgloss.Width(title)))
-	return lipgloss.JoinHorizontal(lipgloss.Center, title, line)
-}
-
-func (model Model) footerView() string {
-	info := infoStyle.Render(fmt.Sprintf("%3.f%%", model.ViewPort.ScrollPercent()*100))
-	line := strings.Repeat("─", max(0, model.ViewPort.Width-lipgloss.Width(info)))
-	return lipgloss.JoinHorizontal(lipgloss.Center, line, info)
+    return output
 }
 
 func saveTasks(model Model){
@@ -138,23 +213,7 @@ func saveTasks(model Model){
 }
 
 func main() {
-    m := Model{}
-
-    // read in the contents of json file or create one if one doesn't exist
-    file, err := os.Open("data.json")
-    if err != nil {
-        err := os.WriteFile("data.json", []byte(""), 0666)
-        if err != nil {
-            log.Fatal(err)
-        } else {
-            m.Items = []Task{}
-        }
-    } else {
-        json.NewDecoder(file).Decode(&m.Items)
-    }
-    file.Close()
-
-    p := tea.NewProgram(m)
+    p := tea.NewProgram(initialModel())
     if _, err := p.Run(); err != nil {
         fmt.Printf("Alas, there's been an error: %v", err)
         os.Exit(1)
